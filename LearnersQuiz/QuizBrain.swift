7f1d199acc53e0ecb6be317bb6b9b69667525a61
Sherry Yuan <sherryuan@sherrys-macbook-pro.local>
2016-05-15 21:09:13 -0700

    Initial Commit

---
+++ LearnersQuiz/QuizBrain.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  LearnersQuiz
+//
+//  Created by Sherry Yuan on 2016-05-15.
+//  Copyright © 2016 Sherry Yuan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class QuizBrain {
+    // Bank of questions, integer would correspond to an image
+    private var questionBank: Dictionary<String, String> = [
+        "stopSign.jpg": "Stop"
+    ]
+    
+    // called by QuizViewController when "Next question" button is clicked
+    private func askQuestion() -> (String, String){
+        // TODO: gets random question from questionBank
+        
+        return ("stopSign.jpg", "Stop")
+    }
+}
---
+++ LearnersQuiz/QuizViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  LearnersQuiz
+//
+//  Created by Sherry Yuan on 2016-05-15.
+//  Copyright © 2016 Sherry Yuan. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    
+
+}
+
